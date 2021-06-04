e1a9f45c9f739a3c892bdea3a9235a483485deec
Mohd Sajjad Hasan <mohdsajjadhasan@gmail.com>
2021-06-04 08:33:03 +0530

    Add about

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,6 @@
 console.log('client side javascipt file is loaded')
 
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
-// fetch('/weather?address=boston').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//         console.log(data.error)
-//         } else {
-//             console.log(data.location)
-//             console.log(data.forecast)
-
-//         }
-//     })
-// })
+// )
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
